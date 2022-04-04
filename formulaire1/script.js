b025702a61c67ab904a93b4ea825cd9ca3391204
Cyril Kamgais <cyrilheike@yahoo.com>
2022-04-04 14:51:42 +0200

    first commit

---
+++ formulaire1/script.js
@@ -0,0 +1,12 @@
+var form = document.getElementById("form").addEventListener("submit", function autrepage(){
+
+localStorage.setItem("username",document.getElementById("username").value) ;
+localStorage.setItem("pseudo",document.getElementById("pseudo").value) ;
+localStorage.setItem("email",document.getElementById("email").value) ;
+
+
+
+
+
+
+})
---
+++ formulaire1/script2.js
@@ -0,0 +1,4 @@
+document.getElementById("username2").innerHTML += localStorage.getItem("username");
+document.getElementById("pseudo2").innerHTML += localStorage.getItem("pseudo");
+document.getElementById("email2").innerHTML += localStorage.getItem("email");
+
